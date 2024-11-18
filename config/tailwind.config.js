6f6be58dc735e66399f322f5802dbfbd52eaac1f
Remy Cassagne <remy.engassac@icloud.com>
2024-11-18 16:17:38 +0100

    add custom tailwind color

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'primary': '#FF385C',
+      },
     },
   },
   plugins: [
