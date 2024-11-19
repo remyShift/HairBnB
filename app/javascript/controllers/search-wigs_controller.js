a93e4af49ca7c72847ca3021e78430f544a87d6f
Lea Gau <157475289+lea3738@users.noreply.github.com>
2024-11-19 17:59:55 +0100

    added seach on locations

--- app/javascript/controllers/search-wigs_controller.js
+++ app/javascript/controllers/search-wigs_controller.js
@@ -3,7 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-wigs"
 export default class extends Controller {
   static targets = [
-    "form"
+    "form",
+    "wigsContainer",
+    "wigName",
+    "wigLocation"
   ]
   connect() {
     console.log("search functionnality");
@@ -11,7 +14,16 @@ export default class extends Controller {
 
   search(event) {
     event.preventDefault();
-    console.log("searching");
-    console.log(this.formTarget);
+    console.log(this.wigNameTarget.value, this.wigLocationTarget.value);
+    const url = `${this.formTarget.action}?product=${this.wigNameTarget.value}&location=${this.wigLocationTarget.value}`
+    fetch(url, {
+      method: "GET", // Could be dynamic with Stimulus values
+      headers: { "Accept": "application/json" }
+    })
+      .then(response => response.json())
+      .then((data) => {
+        this.wigsContainerTarget.innerHTML = "";
+        this.wigsContainerTarget.insertAdjacentHTML('afterbegin', data.filtered_wigs)
+      })
   }
 }
---
+++ app/views/wigs/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.filtered_wigs render(partial: "shared/wigcard", formats: :html, locals: {wigs: @wigs})
