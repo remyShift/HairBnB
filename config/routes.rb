a3061d4a128f6c7ddb37178642ca634c00d719bc
Lea Gau <157475289+lea3738@users.noreply.github.com>
2024-11-19 14:23:59 +0100

    added all routes to wig

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :wigs, only:[:index, :show, :new, :create]
+  resources :wigs
   root to: "wigs#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
