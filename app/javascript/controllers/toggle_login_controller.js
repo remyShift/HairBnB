6136bed971a8ef68493af7d94c050a162da44b0d
Remy Cassagne <remy.engassac@icloud.com>
2024-11-19 14:18:12 +0100

    done modal signup and login

--- app/javascript/controllers/toggle_login_controller.js
+++ app/javascript/controllers/toggle_login_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-login"
 export default class extends Controller {
-  static targets = ["loginSignup", "overlay", "modal"];
+  static targets = ["loginSignup", "overlay", "modalSignup", "modalLogin"];
 
   connect() {
   }
@@ -16,24 +16,45 @@ export default class extends Controller {
   }
 
   toggleOverlay() {
-    this.toggle()
     this.overlayTarget.classList.toggle("hidden")
     this.overlayTarget.classList.toggle("flex")
+  }
+
+  toggleOverlaySignup() {
+    this.toggle()
+    this.toggleOverlay()
     
     setTimeout(() => {
       this.overlayTarget.classList.toggle("opacity-0")
-      this.modalTarget.classList.toggle("translate-y-10")
+      this.modalSignupTarget.classList.remove("hidden")
+      this.modalSignupTarget.classList.toggle("translate-y-10")
+      this.modalLoginTarget.classList.add("hidden")
+    }, 10)
+  }
+
+  toggleOverlayLogin() {
+    this.toggle()
+    this.toggleOverlay()
+    
+    setTimeout(() => {
+      this.overlayTarget.classList.toggle("opacity-0")
+      this.modalLoginTarget.classList.remove("hidden")
+      this.modalLoginTarget.classList.toggle("translate-y-10")
+      this.modalSignupTarget.classList.add("hidden")
     }, 10)
   }
 
   closeOnOverlayClick(event) {
     if (event.target === this.overlayTarget) {
       this.overlayTarget.classList.add("opacity-0")
-      this.modalTarget.classList.add("translate-y-10")
+      this.modalSignupTarget.classList.add("translate-y-10")
+      this.modalLoginTarget.classList.add("translate-y-10")
       
       setTimeout(() => {
         this.overlayTarget.classList.remove("flex")
         this.overlayTarget.classList.add("hidden")
+        this.modalSignupTarget.classList.add("hidden")
+        this.modalLoginTarget.classList.add("hidden")
         this.toggle()
       }, 300)
     }
