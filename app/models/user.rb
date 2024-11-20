c56381106a65c64aed760607d13e4e6b626c9992
Remy Cassagne <remy.engassac@icloud.com>
2024-11-20 14:49:54 +0100

    cloudinary setup and work

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
+
   validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
 end
--- app/models/wig.rb
+++ app/models/wig.rb
@@ -1,3 +1,5 @@
 class Wig < ApplicationRecord
+  has_one_attached :wig_image
+
   validates :name, :material, :color, :hair_style, :length, :address, :price, :image, presence: true
 end
