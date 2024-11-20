5286ce264f2acf585c89610987e46f419c3fc910
Remi Roset <remi.roset.pro@gmail.com>
2024-11-20 12:31:33 +0100

    reviews controller db et view partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_125819) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_163619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.float "rating"
+    t.bigint "user_id", null: false
+    t.bigint "wig_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["wig_id"], name: "index_reviews_on_wig_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,4 +52,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_125819) do
     t.string "image"
   end
 
+  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "wigs"
 end
