c3cc43beb0c29e9c2840dd96bcfa8f6dee23ae91
Remi Roset <remi.roset.pro@gmail.com>
2024-11-21 13:51:17 +0100

    reviews controller action done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,27 @@
 class ReviewsController < ApplicationController
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @reviews = Review.all
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+                                :rating,
+                                :comment
+                              )
   end
 end
