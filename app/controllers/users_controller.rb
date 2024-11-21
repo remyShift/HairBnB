e4c967a5d0ad59ef6ed8457855bade0c78cd1791
Lea Gau <157475289+lea3738@users.noreply.github.com>
2024-11-21 17:20:58 +0100

    corrected render error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
 
   def show
     @wigs = current_user.wigs
+    @bookings = current_user.bookings
   end
 
   private
 
   def set_user
     @user = current_user
-    @bookings =  @user.bookings
+    @bookings = []
     @wigs =[]
   end
 
